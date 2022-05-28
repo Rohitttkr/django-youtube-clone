f0ee2b5a4eca31643511f9c378399611c064daa8
Rohitttkr <106422915+Rohitttkr@users.noreply.github.com>
2022-05-27 22:42:22 -0900

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -5,14 +5,14 @@ from django.urls import include, path
 from app import urls
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
-from django.views.static import serve
+# from django.conf.urls import url
+# from django.views.static import serve
 urlpatterns = [
     path('', include(urls)),
 
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+#     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+#     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
